b47ea5ff163868506eefd5a4ab3a4e1378927799
abadila <alex.badila@mail.utoronto.ca>
2016-01-17 11:49:29 -0500

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 def gradient_descent(x, y, eta):
-    N = x.shape[1]
-    print N
-    J = y.shape[0]
-    w = np.random.random((J, 1))
-    delta = np.zeros((J, 1))
-    y_hat = np.zeros((J, 1))
+    xd = np.shape(x)
+    yd = np.shape(y)
+    N = xd[1]
+    T = yd[0]
+    w = np.random.rand(N+1)
+    delta = np.zeros(N+1, dtype=float)
+    y_hat = np.zeros(T, dtype=float)
     NUM_IT = 10000
     for l in range(0, NUM_IT):
-        for k in range(1, N + 1):
+        for k in range(0, T):
             y_hat[k] = w[0]
-            for n in range(1, N + 1):
-                for i in range(1, N + 1):
-                    y_hat[k] = y_hat[k] + w[i] * x[n, i]
-                for n in range(1, N + 1):            
-                    delta[0] = delta[0] + 2 / N * (y[n] - y_hat[n])             
             for i in range(1, N + 1):
-                for n in range(1, N + 1):
-                    delta[i] = delta[i] + 2 / N * (y[n] - y_hat[n]) * x[n, i]
-            for i in range(0, N + 1):
-                w[i] = w[i] + eta * delta[i]
+                y_hat[k] = y_hat[k] + w[i] * x[k, i-1]
+        for k in range(0, T):            
+            delta[0] = delta[0] + (2 * (y[k,0] - y_hat[k]))/T             
+        for i in range(1, N+1):
+            for k in range(0, T):           
+                delta[i] = delta[i] + (2 * (y[k,0] - y_hat[k])*x[k, i-1])/T            
+        for i in range(0, N + 1):
+            w[i] = w[i] + eta * delta[i]
     return w            
