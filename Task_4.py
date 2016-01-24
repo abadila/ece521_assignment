8434a424b77341722e64a0308c897b745bb298ed
abadila <alex.badila@mail.utoronto.ca>
2016-01-24 14:39:49 -0500

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from gradient_descent import gradient_descent
+from reg_gradient_descent import reg_gradient_descent
 
+NUM_IT = 1000
 
 train_x_1 = np.linspace (1.0 , 10.0 , num =100) [:, np. newaxis]
 train_x_2 = [x**2 for x in train_x_1]
@@ -39,9 +40,9 @@ train_x[:, 3] = train_x_4_n
 train_x[:, 4] = train_x_5_n
 
 train_y = np.sin( train_x_1 ) + np.power ( train_x_1 , 2) * 0.1 + np.random .randn (100 , 1) * 0.5
-eta = 0.001
+eta = 0.01
 plt.plot(train_x_1, train_y)
-w = gradient_descent(train_x, train_y, eta)
+w = reg_gradient_descent(train_x, train_y, eta, 0, NUM_IT)
 train_y_p = np.zeros(100)
 for i in range(0, 100):
     train_y_p[i] = w[0]
@@ -50,4 +51,3 @@ for i in range(0, 100):
 
 plt.plot(train_x_1, train_y_p)
 plt.show()
-
