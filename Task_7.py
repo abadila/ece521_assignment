ff7ee61cb2a1ea01ed97f9b4fa71837a57e6823f
abadila <alex.badila@mail.utoronto.ca>
2016-01-23 21:22:57 -0500

    Update Task_7.py

--- Task_7.py
+++ Task_7.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from reg_gradient_descent import reg_gradient_descent
 
 with np.load ("TINY_MNIST.npz") as data :
@@ -9,19 +10,21 @@ train_t = t[0:50]
 lamda = [0, 0.0001, 0.001, 0.01, 0.1, 0.5]
 lamda_shape = np.shape(lamda)
 train_x_shape = np.shape(train_x)
-T = train_x_shape[0]
 L = lamda_shape[0]
 N = train_x_shape[1]
 eta = 0.01
 error = np.zeros((L, 2))
-#N = w_shape[0] 
-#M = eval_shape[0]  
+NUM_IT = 1000 
 for i in range(0, L): 
-    w = reg_gradient_descent(train_x, train_t, eta, lamda[i])
+    w = reg_gradient_descent(train_x, train_t, eta, lamda[i], NUM_IT)
     error[i, 0] = lamda[i]
-    for j in range(1, N):  
-        for k in range(0, T): 
-            w_shape = np.shape(w)
-            M = w_shape[0]    
-            error[i, 1] = error[i, 1] + (np.dot(w[1:M], x_eval[j]) + w[0] - t_eval[j]) ** 2 
-print error
+    for j in range(0, N):   
+        t_predicted = np.dot(w[1:N+1], np.transpose(x_eval[j, :])) + w[0]  
+        if t_predicted <= 0.5:
+            t_predicted = 0
+        else:
+            t_predicted = 1   
+        if t_predicted != t[j]:
+            error[i, 1] = error[i, 1] + 1              
+print error     
+    
